19719fa1d36228f4224d35d4eec4f46f94202b8f
IhnoH <sample0475@gmail.com>
2022-10-04 17:13:40 +0900

    [Day1] 다리 만들기(2146)_황인호

--- BOJ/2146/2146_황인호.py
+++ BOJ/2146/2146_황인호.py
@@ -1,3 +1,7 @@
+'''
+https://www.acmicpc.net/problem/2146
+'''
+
 from collections import deque
 import numpy as np
 n = int(input())
@@ -22,10 +26,13 @@ while q:
         nx = x + dx[i]
         ny = y + dy[i]
         if nx >= 0 and nx < n and ny >= 0 and ny < n:
+            # 바다를 만나면 해안가라 판단하여 좌표추가
             if m[nx][ny] == 0 and not (x, y) in tmp: tmp.append((x, y))
             if not visited[nx][ny] and m[nx][ny] == 1:
                 q.append((nx, ny))
                 visited[nx][ny] = True
+
+    # q에 좌표가 없다면 해당 섬을 다 방문한 것이라 판단, 다음 섬의 좌표 append
     if not q:
         island.append(tmp)
         tmp = []
@@ -35,10 +42,8 @@ while q:
                     q.append((i, j))
                     visited[i][j] = True
 
-#print(island)
-#print(np.array(visited))
-
 dist = []
+# 각 섬의 해안가 간의 거리 계산
 for i in range(len(island)):
     for j in range(i+1, len(island)):
         [[dist.append(abs(k[0] - t[0]) + abs(k[1] - t[1]) - 1) for k in island[j]] for t in island[i]]
