c4961bfeb941789198e154c3f25a5520b4a43b1c
Kuhy <kwh1019_@naver.com>
2022-10-09 01:30:45 +0900

    [Day5] 별 찍기 - 10(2447)_김우헌

---
+++ baekjoon/2447/2447_김우헌.py
@@ -0,0 +1,42 @@
+'''
+baekjoon g5 별 찍기 - 10
+https://www.acmicpc.net/problem/2447
+111111111 lv3-1
+101101101 lv3-2
+111111111 lv3-3
+111000111 lv
+101000101
+111000101
+111111111
+101101101
+111111111
+
+'''
+
+import sys
+sys.setrecursionlimit(10**7)
+
+input = sys.stdin.readline
+
+N = int(input())
+star = [['*' for _ in range(N)] for _ in range(N)]
+
+def makepattern(star, N, row, col):
+    length = N // 3
+    x = row + length
+    y = col + length
+    for r in range(x, x+length):
+        for c in range(y, y+length):
+            star[r][c] = ' '
+    if length != 1:
+        for r in range(3):
+            for c in range(3):
+                if r == c == 1:
+                    ...
+                else:
+                    makepattern(star, length, row + r*length, col + c*length)
+
+makepattern(star, N, 0, 0)
+
+for s in star:
+    print(''.join(s))
