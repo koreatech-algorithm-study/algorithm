f5c2709a308cc8beb4cb4beb77e148a5436fd1c8
isayaksh <isayaksh@gmail.com>
2022-11-22 15:17:33 +0900

    [Day27] 겹치는 건 싫어(20922)_김상현

--- baekjoon/20922/20922_김상현.py
+++ baekjoon/20922/20922_김상현.py
@@ -4,20 +4,21 @@ from collections import defaultdict
 
 def solution(N, K, A):
     answer = 0
-    left, right = 0, 0
+    start, end = 0, 0
 
     # [1] 수열에 주어진 각 정수의 개수
     numberCount = defaultdict(int)
 
     # [2] 투 포인터
-    while right < N:
-        if numberCount[A[right]] >= K:
-            numberCount[A[left]] -= 1
-            left += 1
+    while end < N:
+        if numberCount[A[end]] >= K:
+            numberCount[A[start]] -= 1
+            start += 1
         else:
-            numberCount[A[right]] += 1
-            right += 1
-            answer = max(answer, right - left)
+            numberCount[A[end]] += 1
+            end += 1
+            answer = max(answer, end - start)
+
     return answer
 
 # input
